107f7ab891d588e4b21db2c61f26d6ee38c81fb4
Sakke <tuomennorosaku@gmail.com>
2023-09-25 21:06:35 +0300

    Task 5.2 Updated

--- src/week5_1/Summation.java
+++ src/week5_1/Summation.java
@@ -2,29 +2,53 @@ package week5_1;
 
 public class Summation extends Thread {
     private int[] numberArray;
-    private int thread;
+    private int startIndex, endIndex;
+    private static int totalSum = 0;
 
-    public Summation(int thread, int[] numberArray) {
-        this.thread = thread;
+    public Summation(int[] numberArray, int startIndex, int endIndex) {
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
         this.numberArray = numberArray;
     }
 
     public void run() {
         int sum = 0;
-        for (int j : numberArray) {
-            sum += j;
-            System.out.println("Thread: " + this.thread + " sum: " + sum);
-
+        for (int i = startIndex; i < endIndex; i++) {
+            sum += numberArray[i];
         }
+        totalSum += sum;
+        System.out.println("Sum of " + startIndex + " to " + endIndex + " is " + sum);
     }
-        public static void main (String[]args){
-            int[] array1 = {1, 4, 5, -5, 10};
 
-            Summation summation1 = new Summation(1, array1);
-            Summation summation2 = new Summation(2, array1);
+    public static void main(String[] args) {
+        int[] numberArray = {5, 5, 10, 2, 6, 10, 20, 50, 20, 10, 5, 20, 2};
+        int numberOfThreads = 5;
+        Thread[] threads = new Thread[numberOfThreads];
 
-            summation1.start();
-            summation2.start();
+        int startI = 0;
+        int endI;
+        int elementsPerThread = numberArray.length / numberOfThreads;
 
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            endI = startI + elementsPerThread;
+
+            if (i == numberOfThreads - 1) {
+                endI = numberArray.length;
+            }
+
+            threads[i] = new Summation(numberArray, startI, endI);
+            threads[i].start();
+            startI = endI;
         }
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+        System.out.println("Total sum is " + totalSum);
     }
+}
